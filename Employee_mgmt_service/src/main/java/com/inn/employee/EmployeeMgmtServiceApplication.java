3dff2e45028bc808b8e4ff20c9658bc09cefc46e
Akash Panchal <akashpanchal9405@gmail.com>
2025-11-10 12:05:51 -0800

    added employee service data

---
+++ Employee_mgmt_service/src/main/java/com/inn/employee/EmployeeMgmtServiceApplication.java
@@ -0,0 +1,13 @@
+package com.inn.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeMgmtServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeMgmtServiceApplication.class, args);
+	}
+
+}
